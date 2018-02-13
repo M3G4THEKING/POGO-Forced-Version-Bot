9e0da265804722062b33f73a24c2c283f023281d
Robert Khousadian <31484519+rkhous@users.noreply.github.com>
2018-02-13 02:06:11 -0800

    switch to rewrite

--- runbotto.py
+++ runbotto.py
@@ -5,26 +5,16 @@ import requests
 import time
 
 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print("Connected!")
-    print("Logged in as: " + client.user.name)
-    print("User ID:" + client.user.id)
-    print("_________________")
-    await client.change_presence(game=discord.Game(name="!version"))
-
-logging.basicConfig(level=logging.ERROR)
 bot = commands.Bot(command_prefix='!')
 
-@client.event
-async def on_message(message):
-    if message.content.startswith("!version"):
-        def version_check(msg):
-            return msg.content.startswith("!version")
-        message = await client.wait_for_message(author=None, check=version_check)
-        res = requests.get('https://pgorelease.nianticlabs.com/plfe/version')
-        await client.send_message(message.channel, ("```\nCurrently Forced API: " + (res.text) + "\n\nSuccesfully requested Niantic Labs at:\n" + str(time.asctime() + "```")))
+logging.basicConfig(level=logging.ERROR)
 
-client.run("------Insert Bot Token Here------")
+@bot.command(pass_context=True)
+async def version(ctx):
+    if ctx:
+        res = requests.get('https://pgorelease.nianticlabs.com/plfe/version')
+        await bot.say("```\nCurrently Forced API: " + (res.text) +
+                      "\n\nSuccesfully requested Niantic Labs at:\n" +
+                      str(time.asctime() + "```"))
+
+bot.run("------Insert Bot Token Here------")
